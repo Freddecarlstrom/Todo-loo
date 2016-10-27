c8b00ed7ff949aa78a2edeaadd31afb18a51e55a
Joy Friberg <joy.friberg@gmail.com>
2016-10-27 20:07:30 +0200

    new todo

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,19 @@
 
 <template>
 	<div>
-		Hello world
+		Todo-loo
+		<new-todo></new-todo>
 	</div>
 </template>
 
 <script>
 	'use strict';
-
+	var newTodo = require('components/new-todo.vue');
 	module.exports = {
+
+		components: {
+			'new-todo': newTodo
+		},
 	}
+
 </script>
---
+++ src/components/new-todo.vue
@@ -0,0 +1,48 @@
+<style>
+    #todo-description{
+        margin-top:5%;
+    }
+</style>
+
+<template>
+    <div class="col-md-offset-6 col-md-6">
+        <h1>Create new todo-loo</h1>
+        <div class="row">
+            <div class="col-md-offset-3 col-md-6">
+                <input class="form-control" v-model="name" placeholder="Name">
+            </div>
+            <div class="col-md-offset-3 col-md-6" id="todo-description">
+                <textarea class="form-control" v-model="description" placeholder="Add description"  rows="10"></textarea>
+            </div>
+            <button class="btn btn-primary" v-on:click="saveTodo">Save</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    'use strict';
+
+    module.exports = {
+        data: function() {
+            return {
+                name: 'Test',
+                description: 'Hej hej'
+            }
+        },
+        methods: {
+            saveTodo: function() {
+                fetch('http://localhost:3004/todos', {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        name: this.name,
+                        description: this.description,
+                    })
+                })
+            }
+        }
+    }
+</script>
